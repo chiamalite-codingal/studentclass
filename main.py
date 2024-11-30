23f17c624f4b2e727981c0fa999083f0f7488fc7
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-11-30 17:28:14 +0100

    student class

---
+++ main.py
@@ -0,0 +1,13 @@
+#create a class
+class girl:
+    def __init__(self,name,color ,age):
+     self.name = name
+     self.color = color
+     self.age = age
+#object creation
+girl1 = girl("niva","red",9)
+girl2 = girl("diva","pink",7)
+#display
+print("i have girl1. the name is",girl1.name)
+print("i have niva and her age is:",girl1.age)
+print("diva's color is:",girl2.color)
